6edb0f510b0f7b4f8f97ed566b5e1167abee89b6
Eduardo-Unicamp <e260057@dac.unicamp.br>
2026-07-12 19:05:15 -0300

    fix: model.Customer and model.Product changes in order to prevent showing the password and to stop requiring an id when registering a new product

--- internal/model/Product.go
+++ internal/model/Product.go
@@ -42,7 +42,6 @@ func NewProduct(name string, price decimal.Decimal, stock int) (*Product, error)
 }
 
 type CreateProductRequest struct {
-	ID    uuid.UUID       `json:"id"`
 	Name  string          `json:"name"`
 	Price decimal.Decimal `json:"price"`
 	Stock int             `json:"stock"`
